1f9e713751154c4e131dd9486a4849ec1ea566d9
NikBrat <nikbratushka@gmail.com>
2023-04-04 11:15:37 +0300

    1 Задание

--- L8b.py
+++ L8b.py
@@ -6,9 +6,10 @@ img = cv.imread('variant-4.jpeg')
 # разбиваем изображение на каналы
 b, g, r = cv.split(img)
 # выводим синий канал
-cv.imshow("Blue channel only", img)
-cv.imshow("Blue channel only1", b)
+cv.imshow("Blue channel only", b)
 cv.waitKey(0)
+cv.destroyAllWindows()
+
 
 
 
